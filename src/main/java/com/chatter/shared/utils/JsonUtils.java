6679a549b2c6fb690ffc7919a88f18110ce3cfe4
Maxim Serov <maxim58r@gmail.com>
2024-12-17 17:05:34 +0300

    assembly and publication of shared-library in Maven

--- src/main/java/com/chatter/shared/utils/JsonUtils.java
+++ src/main/java/com/chatter/shared/utils/JsonUtils.java
@@ -2,10 +2,13 @@ package com.chatter.shared.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class JsonUtils {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    static ObjectMapper objectMapper = new ObjectMapper();
 
     public static String toJson(Object obj) throws JsonProcessingException {
         return objectMapper.writeValueAsString(obj);
